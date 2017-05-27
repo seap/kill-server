ceb63ecf81dd532b110dd4b3a7384952a78a3e47
seayang <ykudos@gmail.com>
2017-05-27 18:21:34 +0800

    update authorize

--- middlewares/authorize.js
+++ middlewares/authorize.js
@@ -1,17 +1,11 @@
 import logger from '../common/logger'
 import codeManager from '../common/codeManager'
 
-const SSO_VALIDATE_API = 'http://sso.haiziwang.com/sso-web/mobileVisit/validate.do'
-
-// 验证用户是否已登录, http://wiki.haiziwang.com/xwiki/bin/view/
 export default async (ctx, next) => {
-
-  console.log('ctx.session1: ', ctx.session)
-  ctx.session.isLogin = true
-  console.log('ctx.session2: ', ctx.session)
-  // if (ctx.session.isLogin) {
+  if (ctx.session.isLogin) {
     await next()
-  //   return
-  // }
-  // ctx.body = Object.assign({}, codeManager.noLogin)
+  } else {
+    logger.log('debug', 'user is not login')
+    ctx.body = Object.assign({}, codeManager.noLogin)
+  }
 }
